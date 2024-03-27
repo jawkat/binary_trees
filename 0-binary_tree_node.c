c8d6048fb7e9d7da006030f8caf9b93a908579ac
jawkat <kad.joliano@gmail.com>
2024-03-27 10:26:44 +0000

    correction betty style task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,6 +11,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
+
 	new_node = malloc(sizeof(binary_tree_t));
 
 	if (!new_node)
@@ -23,5 +24,4 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 
 	return (new_node);
-
 }
